9c7cc5899c87400baf28ead273a9f6213f38047e
Horziox <43693672+Horziox@users.noreply.github.com>
2019-11-18 19:34:19 +0100

    Add files via upload

---
+++ .gitignore/commands/eval.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const { inspect } = require("util");
+module.exports = {
+    name: "eval",
+    description: "Commande secrète...",
+    usage: "<input>",
+    execute(message, args, bot, prefix) {
+        if(message.author.id ==="340212760870649866") {
+            let toEval = args.join(" ");
+            let evaluated = inspect(eval(toEval, {depth: 0}))
+            if(toEval) {
+                let hrStart = process.hrtime()
+                let hrDiff = process.hrtime(hrStart)
+                return message.channel.send(`\`${toEval}\`\n*Exécuté en ${hrDiff[0]>0 ? `${hrDiff[0]}sec` :''}${hrDiff[1]/1000000}ms.*\`\`\`javascript\n${evaluated}\n\`\`\``, {maxlengh: 1900})
+            } else {
+                message.reply("oups, tu as oublié l'eval...")
+            }
+        } else {
+            message.channel.send(`Tu crois vraiment que la commande qui me fait faire n\'importe quoi t'es autorisée ?:joy:`)
+        }
+    } 
+}
---
+++ .gitignore/commands/help.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: "help",
+    description: "Besoin d\'aide ? Cette commande est faite pour toi !",
+    execute(message, args, bot, prefix) {
+
+        let demande = message.content.split(" ").slice(1).join();
+        if (!demande) {
+            let embed = new Discord.RichEmbed()
+            .setAuthor(bot.user.username, bot.user.displayAvatarURL)
+            .setTitle("Liste de mes commandes")
+            .addField("Informations", "`"+prefix+"help` Affiche ce message.\n`"+prefix+"ping` Permet de controler mon ping.\n`"+prefix+"warns` Regarde ton nombre de warns.")
+            .addField("Fun", "`"+prefix+"avatar` récupère ton avatar ou celui de la personne mentionnée/ID.\n`"+prefix+"info` Récupère des infos sur le serveur, toi, moi,...")
+            .setColor("#FFFFF")
+            .setTimestamp()
+            .setFooter(`Commande faite par ${message.author.username}`,`${message.author.displayAvatarURL}`)
+            message.channel.send({embed});
+        }
+        else if (demande==="modo") {
+            let embed = new Discord.RichEmbed()
+            .setAuthor(bot.user.username, bot.user.displayAvatarURL)
+            .setTitle("Commandes Modérateurs")
+            .addField("Supprimer messages","`"+prefix+"sup` Supprime le nombre de msg demandés.", true)
+            .addField("Exclure un membre","`"+prefix+"kick` Exclu la personne mentionnée/ID.", true)
+            .addField("Bannir un membre","`"+prefix+"ban` Banni la personne mentionnée/ID.", true)
+            .addField("Avertir un membre","`"+prefix+"warn` Donne un avertissement à la personne mentionnée/ID.", true)
+            .setColor("#FFFFF")
+            .setTimestamp()
+            .setFooter(`Commande faite par ${message.author.username}`,`${message.author.displayAvatarURL}`)
+            message.channel.send({embed});
+        } else {
+            const data = [];
+            const commandName = args[0].toLowerCase();
+            const command = bot.commands.get(commandName) || bot.fun.get(commandName) || bot.moderation.get(commandName)
+
+            if (!command) {
+			    return message.reply("Ta commande est invalide ou n\'existe pas !:eyes:");
+		    }
+
+		    data.push(`**Nom :** ${command.name}`);
+
+		    if (command.description) data.push(`**Description :** ${command.description}`);
+		    if (command.usage) data.push(`**Utilisation :** ${prefix}${command.name} ${command.usage}`);
+
+		    message.channel.send(data, { split: true });
+        }
+
+    }
+    
+}
