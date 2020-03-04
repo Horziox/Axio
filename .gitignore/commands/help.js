4234301ad2b2fd40c1d7d2f59d9543c5eaf11945
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-04 11:56:33 +0100

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -15,20 +15,6 @@ module.exports = {
             .setTimestamp()
             .setFooter(`Commande faite par ${message.author.username}`,`${message.author.displayAvatarURL}`)
             message.channel.send({embed});
-        }
-        else if (demande==="modo") {
-            let embed = new Discord.RichEmbed()
-            .setAuthor(bot.user.username, bot.user.displayAvatarURL)
-            .setTitle("Commandes Modérateurs")
-	    .addField("Valider un membre", "`"+prefix+"verif` Valide un membre à l\entrée du serveur.", true)
-            .addField("Supprimer messages","`"+prefix+"sup` Supprime le nombre de msg demandés.", true)
-            .addField("Exclure un membre","`"+prefix+"kick` Exclu la personne mentionnée/ID.", true)
-            .addField("Bannir un membre","`"+prefix+"ban` Banni la personne mentionnée/ID.", true)
-            .addField("Avertir un membre","`"+prefix+"warn` Donne un avertissement à la personne mentionnée/ID.", true)
-            .setColor("#FFFFF")
-            .setTimestamp()
-            .setFooter(`Commande faite par ${message.author.username}`,`${message.author.displayAvatarURL}`)
-            message.channel.send({embed});
         } else {
             const data = [];
             const commandName = args[0].toLowerCase();
