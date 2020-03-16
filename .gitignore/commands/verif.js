6bf770fa983b1c49f387a54f7a71d26ac0d8ce0e
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-16 15:05:41 +0100

    Create verif.js

---
+++ .gitignore/commands/verif.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "verif",
+    description: "Valide l\'entrée dans le serveur d'un membre.",
+    usage: "mention/id Prénom Nom",
+    guildOnly: true,
+    execute(message, args, bot, prefix) {
+        message.delete();
+        if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("Tu n\'as pas les permissions pour pouvoir valider un membre !");
+        let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+        if(!member) return message.reply("Merci de mentionner la personne à valider !");
+        let name = message.content.split(" ").slice(2).join(' ');
+        if(!name) return message.reply("Tu as oublier de donner le Prénom et Nom !");
+
+        member.addRole('534802802514460672');
+        member.setNickname(name);
+
+        message.channel.send("**"+name+"** à bien été validé(e) ! :tada:");
+    }
+};
