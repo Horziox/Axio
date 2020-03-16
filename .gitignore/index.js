d43fb309f83ed365b7d7db300c118f393283d97a
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-16 14:23:19 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -34,40 +34,39 @@ bot.on('ready', function() {
 
 bot.on('guildMemberAdd', member=> {
   var msg = [
-    `Oh !\nUn ${member.user} vient d\'apparaitre sur le serveur !:scream:`,
-    `${member.user} vient d\'arriver sur le serveur...\nPlanquez vous ! Vite !`,
-    `Hey salut !:partying_face:\nMais attend ${member.user}...\nT\'as pas oublié la pizza par hasard ?:sweat_smile:`,
-    `${member.user} est là pour tout casser !\nMais n'ayez craintes, je suis là !!!`,
-    `OH MON DIEU !:scream:\nAh bah non... C'est juste ${member.user} qui vient d'arriver, excusez moi...:sweat_smile:`,
-    `${member.user} vient de rejoindre le serveur. Vite ! Faites semblant d'être occupés !`
+	  `Oh !\nUn ${member.user} vient d\'apparaitre sur le serveur !:scream:`,
+	  `Hey salut !:partying_face:\nJ'espère que tu as pensé(e) à la pizza ${member.user} ?:sweat_smile:`,
+	  `Hé ! Écoutez ! ${member.user} nous a rejoint !`,
+	  `Où est ${member.user} ? Dans le serveur !`,
+	  `**Swoooosh** !!! ${member.user} vient juste d'atterrir.`,
+	  `${member.user} vient de rejoindre le serveur ! u.u`,
   ];
-  let bvn = msg[Math.floor(Math.random() * msg.length)];
-  let embed = new Discord.RichEmbed()
-  .setTitle(member.user.tag)
-  .setThumbnail(`${member.user.displayAvatarURL}`)
-  .setDescription(bvn)
-  .addField("Règlement", "<#520225211149320196>")
-  .addField("Vérification", "<#533251614279073794>", true)
-  .setColor("7ED321")
-  .setTimestamp()
-  bot.channels.get("644202587377238023").send({embed});
+	let bvn = msg[Math.floor(Math.random() * msg.length)];
+	let embed = new Discord.RichEmbed()
+	.setTitle(member.user.tag)
+	.setThumbnail(`${member.user.displayAvatarURL}`)
+	.setDescription(bvn)
+	.addField("Règlement", "Un petit tour dans le <#520225211149320196> stp ?")
+	.addField("Vérification", "Pensez à aller dans le <#533251614279073794> agent ! ^^", true)
+	.setColor("7ED321")
+	.setTimestamp()
+	bot.channels.get("644202587377238023").send({embed});
 })
 
 bot.on('guildMemberRemove', member => {
-  var msg = [
-    `**${member.user.tag}** est allé cueillir des fleurs.`,
-    `NON !\n**${member.user.tag}** a appuyé sur le mauvais bouton... Ils croivent à n'importe quoi...:person_facepalming:`,
-    `**${member.user.tag}** a fuit devant ma supériorité !\nMouahahahahaaaa !!!:smiling_imp:`,
-    `**${member.user.tag}** as quitté le serveur.\nMinute ! C'était qui en fait !?:thinking:`,
-    `C'EST BON !!! **${member.user.tag}** vient de quitter le serveur !!!\n**__CHAMPAGNE !!!!__**:partying_face::tada:`,
-  ];
-  let gb = msg[Math.floor(Math.random() * msg.length)];
-  let embed = new Discord.RichEmbed()
-  .setColor('D0021B')
-  .setTitle(member.user.tag)
-  .setThumbnail(`${member.user.displayAvatarURL}`)
-  .setDescription(gb)
-  bot.channels.get("644202587377238023").send({embed});
+	var msg = [
+		`**${member.user.tag}** est allé(e) cueillir des fleurs.`,
+		`NON !\n**${member.user.tag}** a appuyé(e) sur le bouton Quitter le serveur...`,
+		`**${member.user.tag}** a quitté(e) le serveur.\nMinute ! C'était qui en fait !?:thinking:`,
+		`1,2,... 98,99,... C'est bien ce qu'il me semblait !\n**${member.user.tag}** a quitté(e) le serveur !:sob:`,
+	];
+	let gb = msg[Math.floor(Math.random() * msg.length)];
+	let embed = new Discord.RichEmbed()
+	.setColor('D0021B')
+	.setTitle(member.user.tag)
+	.setThumbnail(`${member.user.displayAvatarURL}`)
+	.setDescription(gb)
+	bot.channels.get("644202587377238023").send({embed});
 })
 
 
@@ -110,7 +109,7 @@ bot.on('message', message => {
 });
 
 setInterval(function() {
-  let game = ["TE surveiller ;)",prefix+"help","surveiller le Staff...", "Axio ou Vérité !? :3", "se marrer en solitaire..."]
+  let game = ["TE surveiller ;)",prefix+"help","surveiller le Staff...", "Axio ou Vérité !? :3", "se marrer en solitaire...",]
   let status = game[Math.floor(Math.random() * game.length)];
   bot.user.setActivity(status , {type : "PLAYING"});
 }, 5000)
