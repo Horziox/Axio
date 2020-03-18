d7703a2b1286c8e7c0ea1413c55b068b1d146998
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-18 18:03:56 +0100

    Create ident.js

---
+++ .gitignore/commands/ident.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "identité",
+    description: "Permets de ping le sas-présentation.",
+    execute(message, args, bot, prefix) {
+        message.delete();
+        if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return
+        let embed = new Discord.RichEmbed()
+        .setTitle("Vérification")
+        .setDescription("Pensez à vous identifier en envoyant une photo de votre carte de self ou de votre carnet afin d\'accéder au reste des salons !:wink:")
+        .setColor("#FFFFF")
+        message.channel.send("@everyone", embed);
+    }
+}
