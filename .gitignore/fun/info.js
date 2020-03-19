702eaf060ff12c5327bd155581c27146743cc93d
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-19 21:28:22 +0100

    Add files via upload

--- .gitignore/fun/info.js
+++ .gitignore/fun/info.js
@@ -1,40 +1,40 @@
 const Discord = require("discord.js");
 module.exports = {
     name: "info",
-    isDisable: true,
     description: "Récupère des infos sur le bot, le serveur, votre compte ou celui de la personne mentionnée !",
     usage: "<ordre> (ordre = bot, serveur ou mention utilisateur)",
+    isDisable: true,
     execute(message, args, bot, prefix) {
 
         let info = message.content.split(" ").slice(1).join()
         if(info==="bot") {
-            let embed = new Discord.RichEmbed()
+            let embed = new Discord.MessageEmbed()
             .setAuthor(bot.user.username+" Informations")
-            .setThumbnail(bot.user.displayAvatarURL)
+            .setThumbnail(bot.user.displayAvatarURL())
             .setDescription("Salut !\nJe suis "+bot.user.username+", un bot multi-fonctions dévellopé pour vous rendre service !")
             .addField("Versions Logiciel/Librairies", "NodeJS : ``"+process.version+"``\nDiscord.js : ``"+Discord.version+"``\nrequest : ``2.88.0``\nfs : ``0.0.1``\nutil : ``0.12.1``", true)
             .addField("Utilisation Ressources", "OS : "+process.platform+"\nUtilisation RAM : ``"+`${(process.memoryUsage().heapUsed/1000000).toFixed(2)}`+" Mo``", true)
-            .addField("Ping", "Bot : ``"+`${Date.now() - message.createdTimestamp}`+" ms``\nAPI : ``"+`${Math.round(bot.ping)}`+" ms``", true)
+            .addField("Ping", "Bot : ``"+`${Date.now() - message.createdTimestamp}`+" ms", true)
             .setColor("#FFFFF")
             .setTimestamp()
-            .setFooter(`Commande faite par ${message.author.username}`,`${message.author.displayAvatarURL}`)
+            .setFooter(`Commande faite par ${message.author.username}`, message.author.displayAvatarURL())
             message.channel.send({embed});
         } else if (info==="serveur") {
             
             let event = new Date(message.guild.createdAt);
 
-            let embed = new Discord.RichEmbed()
-            .setThumbnail(message.guild.iconURL)
+            let embed = new Discord.MessageEmbed()
+            .setThumbnail(message.guild.iconURL())
             .setAuthor(message.guild)
             .setTitle("Informations sur le serveur :")
             .addField("Propriétaire :",message.guild.owner, true)
-            .addField("Nombres de membres :",message.guild.members.size, true)
-            .addField("Nombres de roles :",message.guild.roles.size, true)
+            .addField("Nombres de membres :",message.guild.memberCount, true)
+            .addField("Nombres de roles :",(message.guild.roles).lengh, true)
             .addField("Nombres de salons totaux :",message.guild.channels.size)
             .addField("Date de création :", event.getDate()+"/"+event.getMonth()+"/"+event.getFullYear())
             .setTimestamp()
             .setColor("#FFFFF")
-            .setFooter("Commande faite par "+`${message.author.username}`,`${message.author.displayAvatarURL}`)
+            .setFooter(`Commande faite par ${message.author.username}`, message.author.displayAvatarURL())
             message.channel.send({embed});
         } else {
 
@@ -46,8 +46,8 @@ module.exports = {
             
             let event = new Date(info.createdAt);
             
-            let embed = new Discord.RichEmbed()
-            .setThumbnail(info.displayAvatarURL)
+            let embed = new Discord.MessageEmbed()
+            .setThumbnail(info.displayAvatarURL())
             .setTitle("Informations sur votre compte :")
             .addField("Pseudo :",info.username, true)
             .addField("Surnom :", info.nickname ? info.nickname : '*Aucun surnom*', true)
