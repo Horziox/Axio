224a5f5522824313396e7f5d5b66885f30d28ae7
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-19 14:52:45 +0100

    Update ident.js

--- .gitignore/commands/ident.js
+++ .gitignore/commands/ident.js
@@ -7,7 +7,7 @@ module.exports = {
         message.delete();
         if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return
         if(message.channel.id !="533251614279073794") return
-        let embed = new Discord.RichEmbed()
+        let embed = new Discord.messageEmbed()
         .setTitle("Vérification")
         .setDescription("Pensez à vous identifier en envoyant une photo de votre carte de self ou de votre carnet afin d\'accéder au reste des salons !:wink:")
         .setColor("#FFFFF")
