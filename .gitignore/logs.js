bdf3be23528d7eddcb0a8a15e23f6c3980618ff6
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-19 21:18:40 +0100

    Add files via upload

---
+++ .gitignore/logs.js
@@ -0,0 +1,120 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    logs: function(bot) {
+        //Redémarrage
+        bot.on('ready', function() {
+            console.log("Ready")
+            const hook = new Discord.WebhookClient('690172280914837571', 'oZc0Es-Tv4284Ex3-iqE-zehMfzz1WpSEa0l0RLXzU9Li7-Uv1GVJDwrZDP58qZHbuSJ');
+            let embed = new Discord.MessageEmbed()
+            .setAuthor(bot.user.username, bot.user.displayAvatarURL())
+            .setTitle("Redémarrage effectué")
+            .setColor("#00fc58")
+            .setTimestamp()
+            hook.send(embed)
+        });
+        //Message supprimé
+        bot.on('messageDelete', message => {
+            const hook = new Discord.WebhookClient('690173157600002067', 'wFtSugyvvFiQFtkL6D31Urit69Kqs2zfsjxJtFDlswuGCS13bt1exY_ShN9YvsVjARlv');
+            let embed = new Discord.MessageEmbed()
+            .setTitle("Message supprimé")
+            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+            .setDescription(message.content)
+            .addField("Salon", "<#"+message.channel.id+">", true)
+            .addField("Auteur", "<@"+message.author.id+">\n`"+message.author.id+"`", true)
+            .setColor("#d1310d")
+            .setTimestamp()
+            hook.send(embed)
+        });
+        //Message Update
+        bot.on('messageUpdate', (oldMessage, newMessage) => {
+            if(oldMessage.author.bot) return
+            const hook = new Discord.WebhookClient('690173157600002067', 'wFtSugyvvFiQFtkL6D31Urit69Kqs2zfsjxJtFDlswuGCS13bt1exY_ShN9YvsVjARlv');
+            let embed = new Discord.MessageEmbed()
+            .setTitle("Message mis à jour")
+            .setAuthor(oldMessage.author.tag, oldMessage.author.displayAvatarURL())
+            .setDescription("**Ancien :**\n"+oldMessage.content+"\n**Nouveau :**\n"+newMessage.content)
+            .addField("Salon", "<#"+oldMessage.channel.id+">", true)
+            .addField("Auteur", "<@"+oldMessage.author.id+">\n`"+oldMessage.author.id+"`", true)
+            .addField("Message", "[Aller au message]("+newMessage.url+")")
+            .setColor("#0d9dd1")
+            .setTimestamp()
+            hook.send(embed)
+        });
+        //Connexion/Déconnexion/Changement Vocal
+        bot.on('voiceStateUpdate', (oldState, newState) => {
+            
+            const hook = new Discord.WebhookClient('690172809376301121', 'e8_PicGUheF6RuETfhckgYXJZgSbjmg1Kk_DtQ-fEhSdB1gBYSl5QNoLBbZYugr37Ohl');
+        
+            if((oldState.channelID == null || oldState.channelID == undefined) && newState.channelID != null) {
+                let embed = new Discord.MessageEmbed()
+                .setTitle("Connexion Vocale")
+                .setAuthor(oldState.member.user.tag, oldState.member.user.displayAvatarURL())
+                .addField("Salon", newState.channel.name, true)
+                .addField("Membre", "<@"+oldState.member.id+">\n`"+oldState.member.id+"`", true)
+                .setColor("#25e64b")
+                .setTimestamp()
+                hook.send(embed)
+            } else if((newState.channelID == null || newState.channelID == undefined) && oldState.channelID != null) {
+                let embed = new Discord.MessageEmbed()
+                .setTitle("Déconnexion Vocale")
+                .setAuthor(newState.member.user.tag, newState.member.user.displayAvatarURL())
+                .addField("Salon quitté", oldState.channel.name, true)
+                .addField("Membre", "<@"+oldState.member.id+">\n`"+oldState.member.id+"`", true)
+                .setColor("#d1310d")
+                .setTimestamp()
+                hook.send(embed)
+            } else if(oldState.channelID == newState.channelID) return
+            else {
+                let embed = new Discord.MessageEmbed()
+                .setTitle("Changement Salon Vocale")
+                .setAuthor(newState.member.user.tag, newState.member.user.displayAvatarURL())
+                .setDescription("`"+oldState.channel.name+"` -> `"+newState.channel.name+"`")
+                .addField("Membre", "<@"+oldState.member.id+">\n`"+oldState.member.id+"`", true)
+                .setColor("#0d9dd1")
+                .setTimestamp()
+                hook.send(embed)
+            }
+        });
+        //Nouveau Utilisateur
+        bot.on('guildMemberAdd', member => {
+          const hook = new Discord.WebhookClient('690176414342709326', 'yXqenq2CWoXZS2XUTles96LP9mrrpszmT_cZ8nakDLLNP1Ee0ekp1UVJUg9-y8TaRE8k');
+          var msg = [
+            `Oh !\nUn(e) ${member.user} vient d\'apparaitre sur le serveur !:scream:`,
+            `Hey salut !:partying_face:\nJ'espère que tu as pensé(e) à la pizza ${member.user} ?:sweat_smile:`,
+            `Hé ! Écoutez ! ${member.user} nous a rejoint !`,
+            `Où est ${member.user} ? Dans le serveur !`,
+            `**Swoooosh** !!! ${member.user} vient juste d'atterrir.`,
+            `${member.user} vient de rejoindre le serveur ! u.u`,
+          ];
+          let bvn = msg[Math.floor(Math.random() * msg.length)];
+          let embed = new Discord.MessageEmbed()
+          .setTitle(member.user.tag)
+          .setThumbnail(member.user.displayAvatarURL())
+          .setDescription(bvn)
+          .addField("Règlement", "Un petit tour dans le <#520225211149320196> stp ?")
+          .addField("Vérification", "Pensez à aller dans le <#533251614279073794> agent ! ^^", true)
+          .setColor("7ED321")
+          .setTimestamp()
+          hook.send("<@"+member.user.id+">",embed)
+        });
+        //Départ Utilisateur
+        bot.on('guildMemberRemove', member => {
+            const hook = new Discord.WebhookClient('690176414342709326', 'yXqenq2CWoXZS2XUTles96LP9mrrpszmT_cZ8nakDLLNP1Ee0ekp1UVJUg9-y8TaRE8k');
+            var msg = [
+              `**${member.user.tag}** est allé(e) cueillir des fleurs.`,
+              `NON !\n**${member.user.tag}** a appuyé(e) sur le bouton Quitter le serveur...`,
+              `**${member.user.tag}** a quitté(e) le serveur.\nMinute ! C'était qui en fait !?:thinking:`,
+              `1,2,... 98,99,... C'est bien ce qu'il me semblait !\n**${member.user.tag}** a quitté(e) le serveur !:sob:`,
+            ];
+            let gb = msg[Math.floor(Math.random() * msg.length)];
+            let embed = new Discord.MessageEmbed()
+            .setColor('D0021B')
+            .setTitle(member.user.tag)
+            .setThumbnail(member.user.displayAvatarURL())
+            .setDescription(gb)
+            .setTimestamp()
+            hook.send(embed)
+        });
+    }
+}
