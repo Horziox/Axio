08b7f2dba32d017de8d0e8b1d5fcf0e1978f40c9
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-19 10:48:12 +0100

    Update drives.js

--- .gitignore/commands/drives.js
+++ .gitignore/commands/drives.js
@@ -11,8 +11,8 @@ module.exports = {
         .setDescription("Voici la liste des différents drives diponibles !")
         .addField("Informations", "[Infos Parents Eleves](https://drive.google.com/open?id=1eHMP0uFgkN-37IAooy4w_JEP9fl4rllA)\n[Courriers Divers](https://drive.google.com/open?id=1l8HV88ExYSb3DwPz6lhfMDuhQPyzkxdt)")
         .addField("Secondes", "[Secondes 3 et 4 Mars 2020](https://drive.google.com/open?id=1U2xdLxEVD6sog3nbPbelr-I8IbHMqW0b)")
-        .addField("Premières", "[Premières Mars 2020](https://drive.google.com/open?id=1T-NF2Tr6Jd7rpwxmkLP5NMCvc-MvrcFY)", true)
-        .addField("Terminales", "[Courriers Divers](https://drive.google.com/open?id=1rNj8uNBAhPDVeuaL10mzTlNSO1Uckfk6)\n[TS2 et TS3 Mars 2020](https://drive.google.com/open?id=1tFiaWxWvcbPyFC3h8z9VXnfZIbr2qJOo)", true)
+        .addField("Premières", "[Premières Mars 2020](https://drive.google.com/open?id=1T-NF2Tr6Jd7rpwxmkLP5NMCvc-MvrcFY)")
+        .addField("Terminales", "[Courriers Divers](https://drive.google.com/open?id=1rNj8uNBAhPDVeuaL10mzTlNSO1Uckfk6)\n[TS2 et TS3 Mars 2020](https://drive.google.com/open?id=1tFiaWxWvcbPyFC3h8z9VXnfZIbr2qJOo)")
         .setColor("#FFFFF")
         message.channel.send(embed);
     }
