d8548bf08f3c531cb6e2f1021a8378e1061a269c
Horziox <43693672+Horziox@users.noreply.github.com>
2020-03-21 21:28:15 +0100

    Add files via upload

--- .gitignore/commands/verif.js
+++ .gitignore/commands/verif.js
@@ -8,7 +8,7 @@ module.exports = {
     execute(message, args, bot, prefix) {
         message.delete();
         if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("Tu n\'as pas les permissions pour pouvoir valider un membre !");
-        let member = message.guild.member(args[0])
+        let member = message.guild.member(message.mentions.users.first()||args[0])
         if(!member) return message.reply("Merci de mentionner la personne à valider !");
         let name = message.content.split(" ").slice(2).join(' ');
         if(!name) return message.reply("Tu as oublier de donner le Prénom et Nom !");
