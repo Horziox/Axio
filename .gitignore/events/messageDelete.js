67e3a8308ac3cfb1822707aa2a58b23e90605d1d
Horziox <43693672+Horziox@users.noreply.github.com>
2020-06-05 15:38:25 +0200

    Create messageDelete.js

---
+++ .gitignore/events/messageDelete.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+module.exports = (bot, message) => {
+    if(message.channel.id == "689931230568775683") return
+    const hook = new Discord.WebhookClient('690173157600002067', process.env.whMessage);
+    bot.guilds.cache.get("520221628563456000").fetchAuditLogs({type: 'MESSAGE_DELETE'})
+    .then(audit => {
+        var log = audit.entries.first() 
+        sendLogs(log) 
+    })
+    async function sendLogs(log) {
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Message supprimé")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+        .setDescription(message.content)
+        .addField("Salon", "<#"+message.channel.id+">", true)
+        .addField("Auteur", "<@"+message.author.id+">\n`"+message.author.id+"`", true)
+        if(message.author.id != log.executor.id) embed.addField("Modérateur", `${log.executor}\n\`${log.executor.id}\``)
+        embed.setColor("#d1310d")
+        .setTimestamp()
+        hook.send(embed)
+    }
+}
