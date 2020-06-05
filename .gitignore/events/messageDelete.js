41882e0cdc54099dbbdd7b502f939c5b6ecbe163
Horziox <43693672+Horziox@users.noreply.github.com>
2020-06-05 18:05:36 +0200

    Update messageDelete.js

--- .gitignore/events/messageDelete.js
+++ .gitignore/events/messageDelete.js
@@ -3,16 +3,21 @@ module.exports = (bot, message) => {
     if (!message.guild) return
     if(message.channel.id == "689931230568775683") return
     const hook = new Discord.WebhookClient('690173157600002067', process.env.whMessage);
-    var fetchLogs = message.guild.fetchAuditLogs({limit: 1, type: 'MESSAGE_DELETE'})
-    var log = fetchLogs.entries.first() 
-    let embed = new Discord.MessageEmbed()
-    .setTitle("Message supprimé")
-    .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
-    .setDescription(message.content)
-    .addField("Salon", "<#"+message.channel.id+">", true)
-    .addField("Auteur", "<@"+message.author.id+">\n`"+message.author.id+"`", true)
-    .setColor("#d1310d")
-    .setTimestamp()
-    if(log && message.author.id === log.target.id) embed.addField("Supprimé par", `${log.executor}\n\`${log.executor.id}\``)
-    hook.send(embed)
+    bot.guilds.cache.get("551394507007197194").fetchAuditLogs({limit: 1, type: 'MESSAGE_DELETE'})
+    .then(audit => {
+        var log = audit.entries.first() 
+        sendLogs(log) 
+    })
+    async function sendLogs(log) {
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Message supprimé")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+        .setDescription(message.content)
+        .addField("Salon", "<#"+message.channel.id+">", true)
+        .addField("Auteur", "<@"+message.author.id+">\n`"+message.author.id+"`", true)
+        if(message.author.id === log.target.id) embed.addField("Modérateur", `${log.executor}\n\`${log.executor.id}\``)
+        embed.setColor("#d1310d")
+        .setTimestamp()
+        hook.send(embed)
+    }
 }
