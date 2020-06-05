4f47a10ae96cad768bb804d97a91be6fbb776d70
Horziox <43693672+Horziox@users.noreply.github.com>
2020-06-05 17:56:49 +0200

    Update messageDelete.js

--- .gitignore/events/messageDelete.js
+++ .gitignore/events/messageDelete.js
@@ -1,22 +1,18 @@
 const Discord = require("discord.js");
 module.exports = (bot, message) => {
+    if (!message.guild) return
     if(message.channel.id == "689931230568775683") return
     const hook = new Discord.WebhookClient('690173157600002067', process.env.whMessage);
-    bot.guilds.cache.get("520221628563456000").fetchAuditLogs({limit: 1, type: 'MESSAGE_DELETE'})
-    .then(audit => {
-        var log = audit.entries.first() 
-        sendLogs(log) 
-    })
-    async function sendLogs(log) {
-        let embed = new Discord.MessageEmbed()
-        .setTitle("Message supprimé")
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
-        .setDescription(message.content)
-        .addField("Salon", "<#"+message.channel.id+">", true)
-        .addField("Auteur", "<@"+message.author.id+">\n`"+message.author.id+"`", true)
-        if(message.author.id != log.executor.id && message.author.id == log.target.id && message.channel.id == log.id) embed.addField("Supprimé par", `${log.executor}\n\`${log.executor.id}\``)
-        embed.setColor("#d1310d")
-        .setTimestamp()
-        hook.send(embed)
-    }
+    var fetchLogs = message.guild.fetchAuditLogs({limit: 1, type: 'MESSAGE_DELETE'})
+    var log = fetchLogs.entries.first() 
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Message supprimé")
+    .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+    .setDescription(message.content)
+    .addField("Salon", "<#"+message.channel.id+">", true)
+    .addField("Auteur", "<@"+message.author.id+">\n`"+message.author.id+"`", true)
+    .setColor("#d1310d")
+    .setTimestamp()
+    if(log && message.author.id === log.target.id) embed.addField("Supprimé par", `${log.executor}\n\`${log.executor.id}\``)
+    hook.send(embed)
 }
